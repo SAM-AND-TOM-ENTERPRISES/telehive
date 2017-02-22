915196f51e1dffb89af41aff0062f21a9bb4be2a
tomblanch118 <tom.blanch118@googlemail.com>
2017-02-22 12:30:27 +0000

    Improved oh_beehive_rx
    Changed oh_beehive_rx to write binary data to serial

--- oh_behive_rx/oh_behive_rx.ino
+++ oh_behive_rx/oh_behive_rx.ino
@@ -1,6 +1,7 @@
 #include <RFM69.h>
 #include <SPI.h>
 #include <SPIFlash.h>
+#include "oh_beehive_rx.h"
 
 #define NODEID      1
 #define NETWORKID   100
@@ -10,97 +11,90 @@
 #define SERIAL_BAUD 115200
 #define ACK_TIME    30  // # of ms to wait for an ack
 
+
 RFM69 radio;
 SPIFlash flash(8, 0xEF30); //EF40 for 16mbit windbond chip
 bool promiscuousMode = false; //set to 'true' to sniff all packets on the same network
-
-
-typedef struct {
-  float t1;
-  float h1;
-  float t2;
-  float h2;
-  float t3;
-  float h3;
-  float t4;
-  float h4;
-  float light;
-} Payload;
 Payload theData;
+
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
   delay(10);
-  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+  radio.initialize(FREQUENCY, NODEID, NETWORKID);
   radio.setHighPower(); //uncomment only for RFM69HW!
   radio.encrypt(KEY);
   radio.promiscuous(promiscuousMode);
-  char buff[50];
-  sprintf(buff, "\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
-  Serial.println(buff);
+  
+  //TODO: This stuff probably wants reporting to some form of debug?
+  // char buff[50];
+  // sprintf(buff, "\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  // Serial.println(buff);
+  
+  
   if (flash.initialize())
-    Serial.println("SPI Flash Init OK!");
+  {
+    //Serial.println("SPI Flash Init OK!");
+  }
   else
-    Serial.println("SPI Flash Init FAIL! (is chip present?)");
+  {
+    //Serial.println("SPI Flash Init FAIL! (is chip present?)");
+  }
 }
 
 
 void loop() {
   if (radio.receiveDone())
   {
-    Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");
-    Serial.print(" [RX_RSSI:");Serial.print(radio.readRSSI());Serial.print("]");
-    if (promiscuousMode)
-    {
-      Serial.print("to [");Serial.print(radio.TARGETID, DEC);Serial.print("] ");
-    }
-
-    if (radio.DATALEN != sizeof(Payload))
-      Serial.print("Invalid payload received, not matching Payload struct!");
-    else
+    if (radio.DATALEN == sizeof(Payload))
     {
       theData = *(Payload*)radio.DATA; //assume radio.DATA actually contains our struct and not something else
-      printDataPacket(&theData);
+      writeDataPacket(&theData);
     }
-    
+    else
+    {
+      //TODO: REPORT ERROR
+      // Serial.print("Invalid payload received, not matching Payload struct!");
+    }
+
     if (radio.ACKRequested())
     {
-      byte theNodeID = radio.SENDERID;
+      //   byte theNodeID = radio.SENDERID;
       radio.sendACK();
-      Serial.print("ACK sent.");
-
     }
-    
-    Blink(LED,3);
+
+    //Blink(LED,3);
   }
 }
 
-static void printDataPacket(Payload * pld)
+//TODO: Candidate for generalising
+/**
+ * Writes a Data Packet to the serial in byte form. 
+ */
+static void writeDataPacket(Payload * pld)
 {
-  Serial.print(pld->light);
-  Serial.print(",");
-  Serial.print(pld->h1);
-  Serial.print(",");
-  Serial.print(pld->t1);
-  Serial.print(",");
-  Serial.print(pld->h2);
-  Serial.print(",");
-  Serial.print(pld->t2);
-  Serial.print(",");
-  Serial.print(pld->h3);
-  Serial.print(",");
-  Serial.print(pld->t3);
-  Serial.print(",");
-  Serial.print(pld->h4);
-  Serial.print(",");
-  Serial.println(pld->t4);
-  
+  //Get pointer to struct & size
+  uint8_t * dp = (uint8_t *)pld;
+  size_t sz = sizeof(Payload);
+
+  //Write Packet Type
+  Serial.write(PKT_TMPHMD);
+
+  //Write Struct
+  for (int i = 0; i < sz; i++)
+  {
+    Serial.write((uint8_t)*dp);
+    dp++;
+  }
 }
 
+/**
+ * Generic shitty, blocking led blinky function
+ */
 void Blink(byte PIN, int DELAY_MS)
 {
   pinMode(PIN, OUTPUT);
-  digitalWrite(PIN,HIGH);
+  digitalWrite(PIN, HIGH);
   delay(DELAY_MS);
-  digitalWrite(PIN,LOW);
+  digitalWrite(PIN, LOW);
 }
